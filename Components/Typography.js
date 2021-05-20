ec0eb1e7d61384cf57ed97f36c0146e5f50872da
Pratham Meghani <premmeghani16@gmail.com>
2021-05-20 13:49:58 +0530

    Component:Typography

---
+++ Components/Typography.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import {H1,H2,H3} from 'native-base'
+const Typography = () => {
+    return (
+        <View>
+            <H1>Aaimaa Web Solutions</H1>
+            <H2>Aaimaa Web Solutions</H2>
+            <H3>Aaimaa Web Solutions</H3>
+
+        </View>
+    )
+}
+
+export default Typography
+
+const styles = StyleSheet.create({})
